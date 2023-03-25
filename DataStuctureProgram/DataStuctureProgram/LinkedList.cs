b0210f24e14aa2cc1611a71ecf984d99817fc134
Mankar123sayu <sayumankar77@gmail.com>
2023-03-25 15:19:44 +0530

    AddFile

---
+++ DataStuctureProgram/DataStuctureProgram/LinkedList.cs
@@ -0,0 +1,29 @@
+﻿using DataStructureProgram;
+
+namespace DataStructureProblem
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+    }
+}
+
+
---
+++ DataStuctureProgram/DataStuctureProgram/Program.cs
@@ -0,0 +1,43 @@
+﻿using DataStructureProblem;
+using System;
+using System.Collections.Generic;
+
+namespace DataStructire
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList Data = new LinkedList();
+            Data.Add(56);
+            Data.Add(30);
+            Data.Add(70);
+           // Data.Display();
+
+            //Data.AddInReverseOrder(56);
+            // Data.AddInReverseOrder(30);
+            // Data.AddInReverseOrder(70);
+            //Data.Display();
+
+            // Data.InsertAtParticularPosition(1, 30);
+            // Data.Display();
+
+            // Data.RemoveFirstNode();
+            // Data.Display();
+
+            //Data.RemoveLastNode();
+            //Data.Display();
+
+            //Data.Search(56);
+            //Data.Display();
+
+           // int position = Data.Search(30);
+           // Data.InsertAtParticularPosition(position + 1, 40);
+            //Data.Display();
+            //Console.WriteLine();
+            //int searchDelete = Data.Search(40);
+            //Data.DeleteNodeAtParticularPosition(searchDelete);
+            //Data.Display();
+        }
+    }
+}
